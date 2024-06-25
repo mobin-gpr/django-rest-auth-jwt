4a0531b8866fa4b83e49e124be5ff96b242eb582
mobin-gpr <mohammad.mobin.ghanbarpour79@gmail.com>
2024-06-25 12:03:53 +0330

    use .env for settings, install django 4.2 lts

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-97f3rjkpp(^)wf6$mvy!(-&dna8@(ft%+y&=2zx=_ttkt+n!_%"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -139,7 +140,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     "https://example.com",  # Add your domain(s) here
     "http://localhost:8000",  # Add your development server domain
-    "http://localhost:3000",  # Add your Next.js Development server domain
+    "http://localhost:3000",  # Add your front-end Development server domain
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
