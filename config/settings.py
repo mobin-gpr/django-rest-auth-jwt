65b32b6303382a1831313c3f114a073c1a0a1f86
mobin-gpr <mohammad.mobin.ghanbarpour79@gmail.com>
2024-06-25 12:07:16 +0330

    debug .env file

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
